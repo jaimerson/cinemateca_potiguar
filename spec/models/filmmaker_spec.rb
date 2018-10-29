180db6cae68b08d6f4f4213228f31317279ce417
Jaimerson Araújo <jaimerson@toptal.com>
2018-10-29 16:52:43 -0300

    Add specs

---
+++ spec/models/filmmaker_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Filmmaker do
+  describe 'validations' do
+    it 'is not valid without name' do
+      expect(described_class.new).not_to be_valid
+    end
+
+    it 'is valid with name' do
+      expect(described_class.new(name: 'Tarantino')).to be_valid
+    end
+  end
+end
